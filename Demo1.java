1e083b128dbb0924574a918826d985523c2574df
zhoubin1986 <985619620@qq.com>
2022-04-27 14:25:39 +0800

    2

---
+++ Demo1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Demo1{
+	public static void main(String[] args) {
+
+		/* byte i = (byte)(-3 >>> 1);
+		System.out.println(i);
+		char c = 'a';
+		byte j = (byte)c; */
+		Scanner s = new Scanner(System.in);
+		// int score1 = s.nextInt();  //获取整数
+		// double score2 = s.nextDouble();  //获取小数
+		String  str = s.next();  //获取字符串
+		
+		// System.out.println(score1 < 60?"等级为C":(score1 >= 80?"等级为A":"等级为B"));
+
+	}
+}
+
